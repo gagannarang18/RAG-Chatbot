c1abd5014a4ef75edaa32427255af2651991ae8e
Gagan Narang <gagannarang0804@gmail.com>
2025-05-20 12:56:14 +0530

    intialize the function of chatbot

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -9,3 +9,20 @@ from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.embeddings import HuggingFaceEmbeddings
+
+# Load environment variables from .env file
+load_dotenv()
+
+class RAGChatbot:
+    
+     def __init__(self, dataset_path="ai_faq.csv", embedding_model="sentence-transformers/all-mpnet-base-v2", llm_model="llama-3.1-8b-instant", temperature=0.7):
+         
+        #  initialize the chatbot with the dataset path, embedding model, LLM model, and temperature
+         self.dataset_path = dataset_path
+         self.embedding_model = embedding_model
+         self.llm_model = llm_model
+         self.temperature = temperature
+         self.vector_store = None
+         self.retriever = None
+         self.chain = None
+    
